72ca5ecb02f45d50b2276b856edecc436568e403
Tsurogane <pmvdossantos@gmail.com>
2024-05-02 17:01:16 -0300

    feat:Exemplo

---
+++ script.js
@@ -0,0 +1,19 @@
+//Escreva um pograma que solicite o nome do usuário e lhe de boas vindas. exibindo uma mensagem pelo console.
+
+//let nome = prompt("Olá. digite seu nome")
+
+// Concatenação
+//console.log("Seja bem vindo " + nome + "!!!")
+
+let pontos = 100
+
+// parseInt () Transforma  o que esta entre () em numero inteiro.
+let bonus = parseInt( prompt("Quantos pontos voce deseja somar?") )
+
+//parseFloat() Transforma o que esta entre () em Numero Racional (numero com . (quebrado))
+console.log(pontos + bonus)
+
+// Adição num 1 + num 2
+// Subtração num 1 - num 2
+// Multiplicação num 1 * num 2
+// Divisão num 1 / num 2
