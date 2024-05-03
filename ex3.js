a94de43ded912fa661b9b38a7bb17b5abec5a9c3
Tsurogane <pmvdossantos@gmail.com>
2024-05-03 15:36:36 -0300

    feat:Exercicio 3

---
+++ ex3.js
@@ -0,0 +1,12 @@
+// Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let n = prompt("Digite um numero");
+
+
+if(n % 2 == 0){
+    alert("Par");
+}
+if(n % 2 == 1){
+    alert("Impar");
+}
+alert;
