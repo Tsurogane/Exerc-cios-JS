deb7b2d74563b1cf39380776af9056c44f48e5bc
Tsurogane <pmvdossantos@gmail.com>
2024-05-06 16:58:12 -0300

    feat: Exercicio 7

---
+++ ex8.js
@@ -0,0 +1,21 @@
+// Escreva um programa que solicita ao usuário seu peso em quilogramas e sua altura em metros. Em seguida, calcule
+//  o Índice de Massa Corporal (IMC) utilizando a fórmula: IMC = peso / (altura * altura). Com base no resultado, 
+//  exiba uma mensagem indicando a faixa de peso em que o usuário se encontra, de acordo com a tabela a 
+//  seguir:
+// IMC menor que 18.5: Abaixo do peso
+// IMC de 18.5 a 24.9: Peso normal
+// IMC de 25.0 a 29.9: Sobrepeso
+// IMC de 30.0 a 34.9: Obesidade grau 1
+// IMC de 35.0 a 39.9: Obesidade grau 2
+// IMC maior ou igual a 40.0: Obesidade grau 3
+
+let peso = parseFloat(prompt("Informe o seu peso (em kg)"))
+let altura = parseFloat(prompt("Informe sua altura (em metros)"))
+
+let imc = peso / (altura*altura)
+
+console.log("Seu IMC é: " + imc.toFixed(2));
+
+if (imc < 28.5) {
+    console.log("Abaixo do peso");
+}
