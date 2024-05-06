ab458f4dc2633d627611e6912e21ff7bd643ba4b
Tsurogane <pmvdossantos@gmail.com>
2024-05-06 16:58:57 -0300

    feat: Exercicio 8

--- ex8.js
+++ ex8.js
@@ -1,7 +1,10 @@
-// Escreva um programa que solicita ao usuário seu peso em quilogramas e sua altura em metros. Em seguida, calcule
-//  o Índice de Massa Corporal (IMC) utilizando a fórmula: IMC = peso / (altura * altura). Com base no resultado, 
-//  exiba uma mensagem indicando a faixa de peso em que o usuário se encontra, de acordo com a tabela a 
-//  seguir:
+// Exercício 8: Cálculo de IMC
+// Escreva um programa que solicita ao usuário seu peso 
+// em quilogramas e sua altura em metros.
+// Em seguida, calcule o Índice de Massa 
+// Corporal (IMC) utilizando a fórmula: IMC = peso / (altura* altura). 
+// Com base no resultado, exiba uma mensagem indicando a faixa de peso em que o
+// usuário se encontra, de acordo com a tabela a seguir:
 // IMC menor que 18.5: Abaixo do peso
 // IMC de 18.5 a 24.9: Peso normal
 // IMC de 25.0 a 29.9: Sobrepeso
@@ -9,13 +12,34 @@
 // IMC de 35.0 a 39.9: Obesidade grau 2
 // IMC maior ou igual a 40.0: Obesidade grau 3
 
-let peso = parseFloat(prompt("Informe o seu peso (em kg)"))
-let altura = parseFloat(prompt("Informe sua altura (em metros)"))
+let peso = parseFloat(prompt("Informe o seu peso (em kg): "));
 
-let imc = peso / (altura*altura)
+let altura = parseFloat(prompt("Informe o seu altura (em metros): "));
 
-console.log("Seu IMC é: " + imc.toFixed(2));
+let imc = peso / (altura*altura);
 
-if (imc < 28.5) {
+console.log("Seu IMC é:" + imc.toFixed(2));
+
+if(imc < 18.5){
     console.log("Abaixo do peso");
+}
+
+else if( imc>= 18.5 && imc <= 24.9 ){
+    console.log("Peso normal");
+}
+
+else if (imc >= 25.0 && imc <= 29.9){
+    console.log("Sobrepeso");
+}
+
+else if (imc >= 30.0 && imc <= 34.9){
+    console.log("Obsidade grau 1");
+}
+
+else if (imc >= 35.0 && imc <= 39.9){
+    console.log("Obsidade grau 2");
+}
+
+else {
+    console.log("Obsidade grau 3");
 }
