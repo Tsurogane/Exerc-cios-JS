32f6f874034e79ad80dfe7e3290be261e00a5df6
Tsurogane <pmvdossantos@gmail.com>
2024-05-07 16:36:47 -0300

    feat: loop

---
+++ loop/exemplo2.js
@@ -0,0 +1,37 @@
+// Para (For)
+
+//  for(let cont = 10; cont >= 0; cont--) {
+//     console.log(cont);
+//  }
+
+//Enquanto (While)
+
+//  let contador =0;
+
+// while (contador <= 5) {
+//     console.log(contador);
+//     contador =-1
+//  }
+
+//Faça Enquanto(Do While) loop codicional
+
+// let num2 =0
+
+// do {   
+
+//     console.log(num2);
+//     num2+=1;
+    
+// } while (contador < 10);
+
+//ForEach (Para Cada)  percorrer a lista
+
+// let num = [1,2,3,4,5,6,7];
+
+// num.forEach(elemento => {
+//     console.log("O numero é: " + elemento);
+
+// });
+
+
+
