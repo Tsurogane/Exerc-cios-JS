31efd3d8dd77afd911c97253e1db3d6cb593ab9a
Tsurogane <pmvdossantos@gmail.com>
2024-05-09 16:49:16 -0300

    feat:Exemplo 3

---
+++ exemplo3.js
@@ -0,0 +1,31 @@
+// // // //Chamando a função alert
+// // // // alert("Hello World")
+
+// // // //Chamando o metodo log()
+// // // // console.log("Olá Thiago");
+
+// // // //Declaração da função  - Ensinando o computador
+
+// // // function bemVindo(nome) {
+// // //     console.log("Bem Vindo " + nome);
+// // // }
+
+// // // // Chamada da função
+
+// // // bemVindo("Thiago")
+
+// // function somar(num1, num2) {
+// //     console.log(num1 + num2);
+// // }
+
+// // let resultado = somar(6, 1)
+
+// function mostrarSituacacaoAluno(nota1, nota2, nota3) {
+
+//     let somaNotas = nota1 + nota2 + nota3
+
+//     let media = somaNotas / 3
+
+//     console.log(media);
+// }
+
